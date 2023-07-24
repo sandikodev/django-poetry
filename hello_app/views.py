10dff521d2b35e9be7d447aea97f5eeb53f81311
sandikodev <androxoss@hotmail.com>
2023-07-24 09:00:40 +0700

    hello_app with hello views (def-function) as index

--- hello_app/views.py
+++ hello_app/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def hello(request):
+    return HttpResponse('<h1>Hello, Fly!</h1><br/><h2>this is OPINIONATE DJANGO AS YOU GO</h2>')
